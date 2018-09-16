785c968b603abbd1b8d487b758ce4114353a2d7b
Tomas Matousek <tomat@microsoft.com>
2018-09-16 10:07:01 -0700

    Skip 4.7.2 specific test

--- src/Compilers/CSharp/Test/CommandLine/CommandLineTests.cs
+++ src/Compilers/CSharp/Test/CommandLine/CommandLineTests.cs
@@ -9781,7 +9781,7 @@ class C
                 result.Output.Trim());
         }
 
-        [ConditionalFact(typeof(IsEnglishLocal))]
+        [ConditionalFact(typeof(IsEnglishLocal), AlwaysSkip = "TODO")]
         public void LoadingAnalyzerNetStandard13()
         {
             var analyzerFileName = "AnalyzerNS13.dll";
