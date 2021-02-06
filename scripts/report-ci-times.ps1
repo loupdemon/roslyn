9dcf9933e2e0681db221bfc454f122c14861bcad
Rikki Gibson <rigibson@microsoft.com>
2021-02-06 14:02:44 -0800

    measure proportion of jobs waiting for helix vs running builds

--- scripts/report-ci-times.ps1
+++ scripts/report-ci-times.ps1
@@ -4,12 +4,25 @@
 . (Join-Path $PSScriptRoot ".." "eng" "build-utils.ps1")
 
 $roslynPipelineId = "15"
-$minDate = [DateTime]"2021-01-14"
-$maxDate = [DateTime]"2021-01-21"
+$minDate = [DateTime]"2021-01-22"
+$maxDate = [DateTime]"2021-01-29"
 
 $baseURL = "https://dev.azure.com/dnceng/public/_apis/"
 $runsURL = "$baseURL/pipelines/$roslynPipelineId/runs?api-version=6.0-preview.1"
 $buildsURL = "$baseURL/build/builds/"
+
+$helixTestJobs = @(
+    "Test_Windows_Desktop_Debug_32",
+    "Test_Windows_Desktop_Spanish_Debug_32",
+    "Test_Windows_Desktop_Debug_64",
+    "Test_Windows_CoreClr_Debug",
+    "Test_Windows_Desktop_Release_32",
+    "Test_Windows_Desktop_Spanish_Release_32",
+    "Test_Windows_Desktop_Release_64",
+    "Test_Windows_CoreClr_Release",
+    "Test_Linux_Debug",
+    "Test_macOS_Debug"
+);
 
 $wantedRecords = @(
     "Build_Windows_Debug",
@@ -24,44 +37,20 @@ $wantedRecords = @(
     "Test_Windows_Desktop_Spanish_Debug_32",
     "Test_Windows_Desktop_Debug_64",
     "Test_Windows_CoreClr_Debug",
+    "Test_Windows_CoreClr_Debug_Single_Machine",
     "Test_Windows_Desktop_Release_32",
     "Test_Windows_Desktop_Spanish_Release_32",
     "Test_Windows_Desktop_Release_64",
     "Test_Windows_CoreClr_Release",
     "Test_Linux_Debug",
+    "Test_Linux_Debug_Single_Machine",
     "Test_macOS_Debug"
 )
 
-# this determines how the jobs will sort.
-# this helps us put artifact producers next to the consumers in the chart
-$priorities = @{
-    Build_Windows_Debug = 1;
-    Build_Windows_Release = 3;
-    Build_Unix_Debug = 5;
-
-    Correctness_Determinism = 7;
-    Correctness_Build = 7;
-    Correctness_SourceBuild = 7;
-
-    Test_Windows_Desktop_Debug_32 = 2;
-    Test_Windows_Desktop_Spanish_Debug_32 = 2;
-    Test_Windows_Desktop_Debug_64 = 2;
-    Test_Windows_CoreClr_Debug = 2;
-    Test_Windows_Desktop_Release_32 = 4;
-    Test_Windows_Desktop_Spanish_Release_32 = 4;
-    Test_Windows_Desktop_Release_64 = 4;
-    Test_Windows_CoreClr_Release = 4;
-    Test_Linux_Debug = 6;
-    Test_macOS_Debug = 6
-}
-
 class Job {
     [int] $runId
-    [int] $attempt
     [string] $name
-    [TimeSpan] $relativeStart
-    [Nullable[TimeSpan]] $prereqFinish
-    [Nullable[TimeSpan]] $startDelay
+    [string] $kind
     [TimeSpan] $duration
 }
 
@@ -142,25 +131,14 @@ function initialPass() {
 
         Write-Host "Measuring run $($run.id) created at $($run.createdDate) - $($run._links.web.href)"
 
-        $createdJob = [Job]::new()
-        $createdJob.runId = $run.id
-        $createdJob.attempt = 0
-        $createdJob.name = "0_Run_Queued"
-        $createdJob.relativeStart = [TimeSpan]0
-        $createdJob.duration = [DateTime]$run.finishedDate - [DateTime]$run.createdDate
-        $createdJob.startDelay = [TimeSpan]0
-        $allJobs.Add($createdJob) | Out-Null
-
-        $runStartTime = [DateTime]$run.createdDate
-
         foreach ($record in $timeline.records) {
             if ($record.type -eq "job" -and $record.result -eq "succeeded" -and $wantedRecords.Contains($record.name)) {
                 $job = [Job]::new()
                 $job.runId = $run.id;
-                $job.attempt = $record.attempt;
-                $job.name = "$($priorities[$record.name])_$($record.name)";
-                $job.relativeStart = [DateTime]$record.startTime - $runStartTime;
+                $job.name = $record.name;
+                $job.kind = if ($helixTestJobs.Contains($record.name)) { "helix-test" } else { "build" }
                 $job.duration = [DateTime]$record.finishTime - [DateTime]$record.startTime;
+                Write-Host "$($job.kind) job $($job.name) in run $($job.runId) took $($job.duration)"
                 $allJobs.Add($job) | Out-Null
             }
         }
@@ -168,92 +146,22 @@ function initialPass() {
     return $allJobs
 }
 
-# there might be rest API that makes it unnecessary to reverse engineer this stuff
-# but for now this is more convenient.
-$prerequisites = @{
-    "1_Build_Windows_Debug"                   = $null;
-    "3_Build_Windows_Release"                 = $null;
-    "5_Build_Unix_Debug"                      = $null;
 
-    "7_Correctness_Determinism"               = $null;
-    "7_Correctness_Build"                     = $null;
-    "7_Correctness_SourceBuild"               = $null;
-
-    "2_Test_Windows_Desktop_Debug_32"         = "1_Build_Windows_Debug";
-    "2_Test_Windows_Desktop_Spanish_Debug_32" = "1_Build_Windows_Debug";
-    "2_Test_Windows_Desktop_Debug_64"         = "1_Build_Windows_Debug";
-    "2_Test_Windows_CoreClr_Debug"            = "1_Build_Windows_Debug";
-    "4_Test_Windows_Desktop_Release_32"       = "3_Build_Windows_Release";
-    "4_Test_Windows_Desktop_Spanish_Release_32" = "3_Build_Windows_Release";
-    "4_Test_Windows_Desktop_Release_64"       = "3_Build_Windows_Release";
-    "4_Test_Windows_CoreClr_Release"          = "3_Build_Windows_Release";
-    "6_Test_Linux_Debug"                      = "5_Build_Unix_Debug";
-    "6_Test_macOS_Debug"                      = "5_Build_Unix_Debug";
-}
-
-function findPrereq([Job]$job) {
-    $prerequisiteName = $prerequisites[$job.name]
-    if (-not $prerequisiteName) {
-        return $null
-    }
-    foreach ($candidate in $allJobs) {
-        if ($candidate.name -eq $prerequisiteName -and $candidate.runId -eq $job.runId) {
-            return $candidate
-            break
-        }
-    }
-}
-
-function fillPrereqTimes($allJobs) {
-    foreach ($job in $allJobs) {
-        [Job]$prereq = findPrereq($job)
-        if ($prereq) {
-            $job.prereqFinish = $prereq.relativeStart + $prereq.duration
-            $job.startDelay = $job.relativeStart - $job.prereqFinish
-            if ($job.startDelay -lt [TimeSpan]::Zero) {
-                throw "error in $($job.runId) $($job.name): relativeStart $($job.relativeStart) is before prereq $($prereq.name) finish $($job.prereqFinish)"
-            }
-        }
-        else {
-            # this is the Run_Created "job"
-            # we don't use the duration of Run_Created here to determine the job's start delay because
-            # the Run_Created duration represents the entire run time of the job
-            $job.prereqFinish = 0
-            $job.startDelay = $job.relativeStart
-        }
-    }
-}
-
-function getAverageTimes([string]$jobName) {
-    $matchingJobs = $allJobs | Where-Object { $_.name -eq $jobName }
-    function getAverageTime($propName) {
-        $measurement = $matchingJobs | Select-Object -ExpandProperty $propName | Measure-Object -Property Ticks -Average
-        if ($null -ne $measurement) {
-            return [TimeSpan]::new($measurement.Average)
-        } else {
-            return [TimeSpan]::Zero
-        }
-    }
-
-    $averageJob = [Job]::new()
-    $averageJob.runId = 0
-    $averageJob.attempt = 0
-    $averageJob.name = $jobName
-    $averageJob.relativeStart = getAverageTime("relativeStart")
-    $averageJob.prereqFinish = getAverageTime("prereqFinish")
-    $averageJob.startDelay = getAverageTime("startDelay")
-    $averageJob.duration = getAverageTime("duration")
-    return $averageJob
-}
 
 $allJobs = [System.Collections.Generic.List[Job]](initialPass)
-fillPrereqTimes($allJobs)
-$averageJobs = [System.Collections.Generic.List[Job]]::new()
-foreach ($jobName in $prerequisites.Keys) {
-    $averageJobs.Add((getAverageTimes($jobName)))
-}
-$averageJobs.Add((getAverageTimes("0_Run_Created")))
+$buildTime = [TimeSpan]::Zero
+$testTime = [TimeSpan]::Zero
 
-$outDir = Join-Path $ArtifactsDir "ci-times.csv"
-$averageJobs + $allJobs | Sort-Object -Property runId,name | Export-Csv -Path $outDir
-Write-Host "Exported CSV to $outDir"
+foreach ($job in $allJobs) {
+    if ($job.kind -eq "helix-test") {
+        $testTime += $job.duration
+    } elseif ($job.kind -eq "build") {
+        $buildTime += $job.duration
+    }
+}
+
+Write-Host "Build time: $buildTime"
+Write-Host "Helix test time: $testTime"
+
+$proportion = $testTime / ($buildTime + $testTime)
+Write-Host "Proportion of machine time used to run Helix tests: $proportion"
